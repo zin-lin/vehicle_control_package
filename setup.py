3b50117f4b889c025debc72f72e06687f79a9ee7
Damian James <zinlinhtun34@gmail.com>
2024-09-22 00:52:06 +0100

    setup fixed

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import find_packages, setup
-
+import os
+from glob import glob
 package_name = 'vehicle_control_package'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob(os.path.join('launch', '*_launch.*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'vehicle_control=vehicle_control_package.vehicle_control:main'
         ],
     },
 )
